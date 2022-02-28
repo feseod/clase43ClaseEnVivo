aac1221002201c6c43d13d8877b1c3b5b4e8bfc0
feseod <feseod@hotmail.com>
2022-02-28 17:45:39 -0300

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Sidebar from "./Sidebar";
+import'../assets/css/app.css';
+
+function App(){
+  return(
+    <div id="wrapper">
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function NavBar(){
+    return(
+        <React.Fragment>
+            <ul>
+                <li>
+                    <a href="#">inicio</a>
+                </li>
+                <li>
+                    <a href="#">nosotros</a>
+                </li>
+                <li>
+                    <a href="#">servicio</a>
+                </li>
+                <li>
+                    <a href="#">contacto</a>
+                </li>
+            </ul>
+        </React.Fragment>
+    )
+}
+export default NavBar;
