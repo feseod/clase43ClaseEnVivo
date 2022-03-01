b980982bd95b14a9fd49740311d8a0964e56d31a
feseod <feseod@hotmail.com>
2022-02-28 23:46:40 -0300

    casi terminado

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Sidebar from "./Sidebar";
+import ContentWrapper from "./ContentWrapper";
 import'../assets/css/app.css';
 
 function App(){
   return(
     <div id="wrapper">
       <Sidebar/>
+      <ContentWrapper />
     </div>
   )
 }
